5aa200e9b6077c02b0bb3fea6c600d1656f48e35
Max W. Portocarrero <mxportocarrero@gmail.com>
2020-05-01 01:24:08 -0500

    redirecting to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
@@ -45,22 +45,34 @@ class App extends React.Component {
   }
 
   render() {
+    const { currentUser } = this.props;
     return (
       <div>
         <Header />
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+// we destructure our root reducer, for using user reducer only
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 // makkes setCurrentUser available isnide this.props
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -1,10 +1,17 @@
+import { userActionTypes } from "./user.types";
+
 const INITIAL_STATE = {
   currentUser: null,
 };
 
+// state actually represents previous state.
+// it will always show the state before actions was triggered
+// we must return a new object for every action
+// if we dont, react might not update reducer States
+// that is why default return the same object, because we donot need to update any reducer state
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case userActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload,
